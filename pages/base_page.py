509e18a3c611806a93276077086636983339d217
28mirceas <mir.sava@gmail.com>
2026-06-03 20:50:19 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -20,17 +20,19 @@ class BasePage:
 
 
     def click(self, locator):
-        element = self.wait.until(
-            EC.presence_of_element_located(locator)
-        )
-        self.driver.execute_script(
-            "arguments[0].scrollIntoView({block:'center'});",
-            element
-        )
-        self.driver.execute_script(
-            "arguments[0].click();",
-            element
-        )
+        # element = self.wait.until(
+        #     EC.presence_of_element_located(locator)
+        # )
+        # self.driver.execute_script(
+        #     "arguments[0].scrollIntoView({block:'center'});",
+        #     element
+        # )
+        # self.driver.execute_script(
+        #     "arguments[0].click();",
+        #     element
+        # )
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        element.click()
 
 
     def type(self, locator, text):
