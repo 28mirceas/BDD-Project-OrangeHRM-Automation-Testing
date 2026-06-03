59ecf506dc1e17ed91aebae0685cc3bff43a0d28
28mirceas <mir.sava@gmail.com>
2026-06-03 18:39:19 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -10,10 +10,13 @@ class Browser:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        # 1. Adaugă această linie pentru a seta o rezoluție stabilă de monitor virtual
+        chrome_options.add_argument("--window-size=1920,1080")
 
         self.driver = webdriver.Chrome(options=chrome_options)
-
-        self.driver.maximize_window()
+        
+        # 2. Pune un caracter '#' la începutul liniei de mai jos pentru a o comenta
+        #self.driver.maximize_window()
         self.driver.implicitly_wait(5)
 
 
